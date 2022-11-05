b5fa61d45fbaf3483b8ea32b5c3d697a633f7650
luisrodri45 <luisedurodriguez2014@gmail.com>
2022-11-05 19:15:18 -0200

    chore: add login with DB

---
+++ ClasesBase/TrabajarUsuario.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace ClasesBase
+{
+    public class TrabajarUsuario
+    {
+
+        //Verifica el nombre de usuario y contraseña
+        public static bool verificarCredenciales(string nombreUsuario, string password)
+        {
+            // conexion a la base de datos
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.conection);
+
+            //operaciones
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Usuario WHERE nombreUsuario = @username AND password = @password";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@username", nombreUsuario);
+            cmd.Parameters.AddWithValue("@password", password);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            if (dt.Rows.Count == 0) return false;
+            return true;
+        }
+        //Obtener Usuario
+        public static DataTable obtenerUsuario(string nombreUsuario)
+        {
+            // conexion a la base de datos
+            SqlConnection cnn = new SqlConnection(ClasesBase.Properties.Settings.Default.conection);
+
+            //operaciones
+            SqlCommand cmd = new SqlCommand();
+
+            cmd.CommandText = "SELECT * FROM Usuario WHERE nombreUsuario = @username";
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = cnn;
+
+            cmd.Parameters.AddWithValue("@username", nombreUsuario);
+
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return dt;
+        }
+    }
+}
